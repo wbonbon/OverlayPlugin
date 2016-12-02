044304db3904f19596a021769c4be861f2b42fbf
Kuriyama Hibiya <kuriyama@hibiya.moe>
2016-12-02 16:50:45 +0900

    Translate: ko-KR

--- OverlayPlugin.Core/Localization.cs
+++ OverlayPlugin.Core/Localization.cs
@@ -30,25 +30,35 @@ namespace RainbowMage.OverlayPlugin
 
             dict[TextItem.ErrorTitle, ""] = "Error";
             dict[TextItem.ErrorTitle, "ja"] = "エラー";
+            dict[TextItem.ErrorTitle, "ko"] = "에러";
             dict[TextItem.RequiredAssemblyFileNotFound, ""] = "Required assembly file {0} was not found.";
             dict[TextItem.RequiredAssemblyFileNotFound, "ja"] = "アセンブリ {0} が存在しません。";
+            dict[TextItem.RequiredAssemblyFileNotFound, "ko"] = "필요한 어셈블리 파일 {0}을(를) 찾을 수 없습니다.";
             dict[TextItem.RequiredAssemblyFileCannotRead, ""] = "Could not load required assembly file {0}.";
             dict[TextItem.RequiredAssemblyFileCannotRead, "ja"] = "アセンブリ {0} は存在しますが、読み込めません。";
+            dict[TextItem.RequiredAssemblyFileCannotRead, "ko"] = "필요한 어셈블리 파일 {0}을(를) 불러올 수 없습니다.";
             dict[TextItem.RequiredAssemblyFileBlocked, ""] = "Could not load required assembly file {0} due to security reasons. It seems the file has blocked or placed on the untrusted zone (such as network drive).";
             dict[TextItem.RequiredAssemblyFileBlocked, "ja"] = "セキュリティ上の問題からアセンブリ {0} を読み込めません。アセンブリがネットワーク上にあるか、またはブロックされている可能性があります。";
+            dict[TextItem.RequiredAssemblyFileBlocked, "ko"] = "보안 문제로 필요한 어셈블리 파일 {0}을(를) 불러올 수 없습니다. 파일이 차단되어 있거나, 신뢰할 수 없는 장소에 (네트워크 드라이브 등) 놓여져있을 수 있습니다.";
             dict[TextItem.RequiredAssemblyFileException, ""] = "Exception occured when loading required assembly file {0}:\n{1}";
             dict[TextItem.RequiredAssemblyFileException, "ja"] = "アセンブリ {0}の読み込み時に例外が発生しました:\n{1}";
+            dict[TextItem.RequiredAssemblyFileException, "ko"] = "어셈블리 {0}을(를) 불러오는 도중 예외가 발생했습니다:\n{1}";
 
             dict[TextItem.DoNotSort, ""] = "Do not sort";
             dict[TextItem.DoNotSort, "ja"] = "ソートしない";
+            dict[TextItem.DoNotSort, "ko"] = "정렬하지 않음";
             dict[TextItem.SortStringAscending, ""] = "String - Ascending";
             dict[TextItem.SortStringAscending, "ja"] = "文字列 - 昇順";
+            dict[TextItem.SortStringAscending, "ko"] = "문자열 - 오름차순";
             dict[TextItem.SortStringDescending, ""] = "String - Descending";
             dict[TextItem.SortStringDescending, "ja"] = "文字列 - 降順";
+            dict[TextItem.SortStringDescending, "ko"] = "문자열 - 내림차순";
             dict[TextItem.SortNumberAscending, ""] = "Number - Ascending";
             dict[TextItem.SortNumberAscending, "ja"] = "数値 - 昇順";
+            dict[TextItem.SortNumberAscending, "ko"] = "숫자 - 오름차순";
             dict[TextItem.SortNumberDescending, ""] = "Number - Descending";
             dict[TextItem.SortNumberDescending, "ja"] = "数値 - 降順";
+            dict[TextItem.SortNumberDescending, "ko"] = "숫자 - 내림차순";
 
         }
 
