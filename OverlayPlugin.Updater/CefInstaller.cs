9151fb83327996a88a9069493d84bb2d62e69f33
ngld <ngld@tproxy.de>
2019-09-27 08:07:09 +0200

    Release 0.7.0

--- OverlayPlugin.Updater/CefInstaller.cs
+++ OverlayPlugin.Updater/CefInstaller.cs
@@ -11,7 +11,7 @@ namespace RainbowMage.OverlayPlugin.Updater
     public class CefInstaller
     {
         const string CEF_DL = "https://github.com/ngld/OverlayPlugin/releases/download/v0.7.0/CefSharp-{CEF_VERSION}-{ARCH}.DO_NOT_DOWNLOAD";
-        const string CEF_VERSION = "75.1.141.0";
+        const string CEF_VERSION = "75.1.14";
 
         public static async Task<bool> EnsureCef(string cefPath)
         {
