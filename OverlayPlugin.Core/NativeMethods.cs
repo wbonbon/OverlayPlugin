8a950347d7415da56c5d175d7a8190c19f0b1847
qitana <53415856+qitana@users.noreply.github.com>
2019-12-26 21:00:18 +0900

    add OpenProcess and CloseHandle

--- OverlayPlugin.Core/NativeMethods.cs
+++ OverlayPlugin.Core/NativeMethods.cs
@@ -284,8 +284,29 @@ namespace RainbowMage.OverlayPlugin
         public static extern bool PostMessageA(IntPtr hWnd, int nBar, int wParam, int lParam);
         [DllImport("user32.dll")]
         public static extern bool GetScrollRange(IntPtr hWnd, int nBar, out int lpMinPos, out int lpMaxPos);
-
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenProcess(ProcessAccessFlags processAccess, bool bInheritHandle, int processId);
+        [DllImport("kernel32.dll")]
+        public static extern int CloseHandle(IntPtr hObject);
         [DllImport("kernel32.dll")]
         public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, IntPtr nSize, ref IntPtr lpNumberOfBytesRead);
-  }
+    }
+
+    [Flags]
+    public enum ProcessAccessFlags : uint
+    {
+        All = 0x1F0FFF,
+        Terminate = 0x1,
+        CreateThread = 0x2,
+        VirtualMemoryOperation = 0x8,
+        VirtualMemoryRead = 0x10,
+        VirtualMemoryWrite = 0x20,
+        DuplicateHandle = 0x40,
+        CreateProcess = 0x80,
+        SetQuota = 0x100,
+        SetInformation = 0x200,
+        QueryInformation = 0x400,
+        QueryLimitedInformation = 0x1000,
+        Synchronize = 0x100000
+    }
 }
