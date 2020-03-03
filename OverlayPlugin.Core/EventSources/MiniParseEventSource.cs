996ec59b124d52969ede575687d59f9b261694e7
Valarnin <Valarnin@gmail.com>
2020-03-02 17:36:25 -0800

    - Removed unneeded CombatantType map

--- OverlayPlugin.Core/EventSources/MiniParseEventSource.cs
+++ OverlayPlugin.Core/EventSources/MiniParseEventSource.cs
@@ -55,25 +55,6 @@ namespace RainbowMage.OverlayPlugin.EventSources
             "Heading"
         };
 
-        private enum CombatantType : byte
-        {
-            None = 0x00,
-            Player = 0x01,
-            BattleNpc = 0x02,
-            EventNpc = 0x03,
-            Treasure = 0x04,
-            Aetheryte = 0x05,
-            GatheringPoint = 0x06,
-            EventObj = 0x07,
-            MountType = 0x08,
-            Companion = 0x09, // this probably actually means minion
-            Retainer = 0x0A,
-            Area = 0x0B,
-            Housing = 0x0C,
-            Cutscene = 0x0D,
-            CardStand = 0x0E,
-        };
-
         private Dictionary<string, System.Reflection.PropertyInfo> CachedCombatantPropertyInfos
             = new Dictionary<string, System.Reflection.PropertyInfo>();
 
