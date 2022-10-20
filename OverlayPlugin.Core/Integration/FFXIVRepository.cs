7227e21575ba2dcc42a28d733243653183ac2ba8
Adrienne Walker <enne@quisquo.us>
2022-10-20 08:25:09 -0700

    Prevent emitting unnecessary fate/ce lines (#128)
    * Prevent emitting unnecessary fate/ce lines
    
    Fixes #73.
    
    * formatting

--- OverlayPlugin.Core/Integration/FFXIVRepository.cs
+++ OverlayPlugin.Core/Integration/FFXIVRepository.cs
@@ -439,6 +439,13 @@ namespace RainbowMage.OverlayPlugin
                 sub.PartyListChanged += new PartyListChangedDelegate(handler);
         }
 
+        public void RegisterZoneChangeDelegate(Action<uint, string> handler)
+        {
+            var sub = GetSubscription();
+            if (sub != null)
+                sub.ZoneChanged += new ZoneChangedDelegate(handler);
+        }
+
         // ProcessChangedDelegate(Process process)
         public void RegisterProcessChangedHandler(Action<Process> handler)
         {
--- OverlayPlugin.Core/NetworkProcessors/LineFateControl.cs
+++ OverlayPlugin.Core/NetworkProcessors/LineFateControl.cs
@@ -13,11 +13,18 @@ namespace RainbowMage.OverlayPlugin.NetworkProcessors
         private readonly int offsetMessageType;
         private readonly int offsetPacketData;
         private readonly FFXIVRepository ffxiv;
+        // fates<fateID, progress>
+        private static Dictionary<uint, uint> fates = new Dictionary<uint, uint>();
 
         private const ushort FateAddCategory = 0x935;
         private const ushort FateRemoveCategory = 0x936;
         private const ushort FateUpdateCategory = 0x93E;
-        private static readonly List<ushort> FateCategories = new List<ushort>() { FateAddCategory, FateRemoveCategory, FateUpdateCategory };
+        private static readonly Dictionary<uint, string> FateCategories = new Dictionary<uint, string>
+        {
+            [FateAddCategory] = "Add",
+            [FateRemoveCategory] = "Remove",
+            [FateUpdateCategory] = "Update",
+        };
 
         private Func<string, DateTime, bool> logWriter;
 
@@ -26,8 +33,8 @@ namespace RainbowMage.OverlayPlugin.NetworkProcessors
         {
             public ushort category;
             public ushort padding;
-            public uint param1;
-            public uint param2;
+            public uint fateID;
+            public uint progress;
             public uint param3;
             public uint param4;
             public uint param5;
@@ -36,7 +43,7 @@ namespace RainbowMage.OverlayPlugin.NetworkProcessors
 
             public override string ToString()
             {
-                return $"{category:X4}|{padding:X4}|{param1:X8}|{param2:X8}|{param3:X8}|{param4:X8}|{param5:X8}|{param6:X8}|{padding1:X8}";
+                return $"{category:X4}|{padding:X4}|{fateID:X8}|{progress:X8}|{param3:X8}|{param4:X8}|{param5:X8}|{param6:X8}|{padding1:X8}";
             }
         }
 
@@ -77,50 +84,82 @@ namespace RainbowMage.OverlayPlugin.NetworkProcessors
             {
                 logger.Log(LogLevel.Error, Resources.NetworkParserInitException, e);
             }
+
+            ffxiv.RegisterZoneChangeDelegate((zoneID, zoneName) => fates.Clear());
         }
 
         private unsafe void MessageReceived(string id, long epoch, byte[] message)
         {
-            if (message.Length < opcode.size + offsetPacketData)
+            if (opcode == null)
+            {
                 return;
+            }
+
+            if (message.Length < opcode.size + offsetPacketData)
+            {
+                return;
+            }
 
             fixed (byte* buffer = message)
             {
-                if (*(ushort*)&buffer[offsetMessageType] == opcode.opcode)
+                if (*(ushort*)&buffer[offsetMessageType] != opcode.opcode)
                 {
-                    ActorControlSelf_v62 mapEffectPacket = *(ActorControlSelf_v62*)&buffer[offsetPacketData];
-                    if (FateCategories.Contains(mapEffectPacket.category))
-                    {
-                        DateTime serverTime = ffxiv.EpochToDateTime(epoch);
-                        string category = "";
-                        switch (mapEffectPacket.category)
-                        {
-                            case FateAddCategory:
-                                category = "Add";
-                                break;
-                            case FateRemoveCategory:
-                                category = "Remove";
-                                break;
-                            case FateUpdateCategory:
-                                category = "Update";
-                                break;
-                        }
-                        logWriter(
-                            $"{category}|" +
-                            $"{mapEffectPacket.padding:X4}|" +
-                            $"{mapEffectPacket.param1:X8}|" +
-                            $"{mapEffectPacket.param2:X8}|" +
-                            $"{mapEffectPacket.param3:X8}|" +
-                            $"{mapEffectPacket.param4:X8}|" +
-                            $"{mapEffectPacket.param5:X8}|" +
-                            $"{mapEffectPacket.param6:X8}|" +
-                            $"{mapEffectPacket.padding1:X8}",
-                            serverTime
-                        );
-                    }
-
                     return;
                 }
+
+                ActorControlSelf_v62 mapEffectPacket = *(ActorControlSelf_v62*)&buffer[offsetPacketData];
+                string categoryStr;
+                if (!FateCategories.TryGetValue(mapEffectPacket.category, out categoryStr))
+                {
+                    return;
+                }
+
+                var category = mapEffectPacket.category;
+                var fateID = mapEffectPacket.fateID;
+                var progress = mapEffectPacket.progress;
+
+                // Do some basic filtering on fate data to avoid spamming the log needlessly.
+                if (category == FateAddCategory)
+                {
+                    if (fates.ContainsKey(fateID))
+                    {
+                        return;
+                    }
+                    fates.Add(fateID, 0);
+                }
+                else if (category == FateRemoveCategory)
+                {
+                    if (!fates.Remove(fateID))
+                    {
+                        return;
+                    }
+                }
+                else if (category == FateUpdateCategory)
+                {
+                    uint oldProgress;
+                    if (fates.TryGetValue(fateID, out oldProgress))
+                    {
+                        if (progress == oldProgress)
+                        {
+                            return;
+                        }
+                    }
+                    fates[fateID] = progress;
+                }
+
+                DateTime serverTime = ffxiv.EpochToDateTime(epoch);
+                logWriter(
+                    $"{categoryStr}|" +
+                    $"{mapEffectPacket.padding:X4}|" +
+                    $"{mapEffectPacket.fateID:X8}|" +
+                    $"{mapEffectPacket.progress:X8}|" +
+                    $"{mapEffectPacket.param3:X8}|" +
+                    $"{mapEffectPacket.param4:X8}|" +
+                    $"{mapEffectPacket.param5:X8}|" +
+                    $"{mapEffectPacket.param6:X8}|" +
+                    $"{mapEffectPacket.padding1:X8}",
+                    serverTime
+                );
             }
         }
 
