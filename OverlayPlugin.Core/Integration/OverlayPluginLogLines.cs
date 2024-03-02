3fba74be382c91423d54973d1e374693d20e6982
valarnin <valarnin@gmail.com>
2024-03-02 13:31:31 -0500

    Add ActorMove and ActorSetPos lines (#316)

--- OverlayPlugin.Core/Integration/OverlayPluginLogLines.cs
+++ OverlayPlugin.Core/Integration/OverlayPluginLogLines.cs
@@ -31,6 +31,8 @@ namespace RainbowMage.OverlayPlugin
             container.Register(new LineBattleTalk2(container));
             container.Register(new LineCountdown(container));
             container.Register(new LineCountdownCancel(container));
+            container.Register(new LineActorMove(container));
+            container.Register(new LineActorSetPos(container));
         }
     }
 
