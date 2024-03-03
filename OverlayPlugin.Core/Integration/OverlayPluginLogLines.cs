db75ce8d9646802e477db7030bd8ee445e59c03c
valarnin <valarnin@gmail.com>
2024-03-02 19:29:15 -0500

    Add `ActorControlExtra` and `SpawnNpcExtra` lines (#318)
    Co-authored-by: Adrienne Walker <enne@quisquo.us>

--- OverlayPlugin.Core/Integration/OverlayPluginLogLines.cs
+++ OverlayPlugin.Core/Integration/OverlayPluginLogLines.cs
@@ -33,6 +33,8 @@ namespace RainbowMage.OverlayPlugin
             container.Register(new LineCountdownCancel(container));
             container.Register(new LineActorMove(container));
             container.Register(new LineActorSetPos(container));
+            container.Register(new LineSpawnNpcExtra(container));
+            container.Register(new LineActorControlExtra(container));
         }
     }
 
