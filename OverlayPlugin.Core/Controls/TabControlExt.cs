718a366ee86d73962d4d166c284a3b1c07a241c5
EQAditu <52854036+EQAditu@users.noreply.github.com>
2024-08-10 16:12:53 -0400

    HiDPI fixes for tab control (#388)
    Fixed tab sizing/painting when using higher than 100% scale.  Requires ACT 3.8.0.x for scale detection.

--- OverlayPlugin.Core/Controls/TabControlExt.cs
+++ OverlayPlugin.Core/Controls/TabControlExt.cs
@@ -1,16 +1,44 @@
 ﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
+using Advanced_Combat_Tracker;
 
 namespace RainbowMage.OverlayPlugin
 {
     public class TabControlExt : TabControl
     {
+        float dpiScale = 0;
+        float DpiScale
+        {
+            get
+            {
+                if (dpiScale == 0)
+                {
+                    try { dpiScale = ActGlobals.oFormActMain.DpiScale; }
+                    catch { dpiScale = 1; }
+                }
+                return dpiScale;
+            }
+        }
+        int Dpi(float InputValue)
+        {
+            return (int)(InputValue * DpiScale);
+        }
+
+        bool itemSizeSet = false;
         protected override void OnPaint(PaintEventArgs e)
         {
-            base.OnPaint(e);
+            if (itemSizeSet == false)   // For some reason setting the size in the constructor is ineffective
+            {
+                itemSizeSet = true;
+                ItemSize = new Size(Dpi(46), Dpi(140));
+            }
+
+            //base.OnPaint(e);    // Seems unnecessary since the next line wipes everything?
             e.Graphics.Clear(SystemColors.ControlLightLight);
-            e.Graphics.FillRectangle(SystemBrushes.ControlLight, 4, 4, (ItemSize.Height * RowCount) - 4, Height - 8);
+            Rectangle tabsetRect = new Rectangle(Dpi(4), Dpi(4), (ItemSize.Height * RowCount) - Dpi(4), Height - Dpi(8));   // The entire tabset area
+            Rectangle tabmodelRect = new Rectangle(tabsetRect.X + Dpi(2), 0, tabsetRect.Width - Dpi(4), 20);    // A size model for a single tab
+            e.Graphics.FillRectangle(SystemBrushes.ControlLight, tabsetRect);
 
             int inc = 0;
 
@@ -18,11 +46,11 @@ namespace RainbowMage.OverlayPlugin
             {
                 Color fore = Color.Black;
                 Font fontF = Font;
-                Font fontFSmall = new Font(Font.FontFamily, (float)(Font.Size * 0.85));
-                Rectangle tabrect = GetTabRect(inc);
-                Rectangle rect = new Rectangle(tabrect.X + 4, tabrect.Y + 4, tabrect.Width - 8, tabrect.Height - 2);
-                Rectangle textrect1 = new Rectangle(tabrect.X + 4, tabrect.Y + 4, tabrect.Width - 8, tabrect.Height - 20);
-                Rectangle textrect2 = new Rectangle(tabrect.X + 4, tabrect.Y + 20, tabrect.Width - 8, tabrect.Height - 20);
+                Font fontFSmall = new Font(Font.FontFamily, (float)(Font.Size * 0.85));  // This is already DPI scaled by Windows because this.Font was
+                Rectangle tabclipRect = GetTabRect(inc);    // A clipping rectangle that encompasses this tab
+                Rectangle tabRect = new Rectangle(tabmodelRect.X, tabclipRect.Y + Dpi(5), tabmodelRect.Width, tabclipRect.Height - Dpi(2)); // A combination of our tab size model and the offset from the clipping rectangle
+                Rectangle textRect1 = new Rectangle(tabmodelRect.X, tabclipRect.Y + Dpi(6), tabmodelRect.Width, tabclipRect.Height - Dpi(20));
+                Rectangle textRect2 = new Rectangle(tabmodelRect.X, tabclipRect.Y + Dpi(22), tabmodelRect.Width, tabclipRect.Height - Dpi(20));
 
                 StringFormat sf = new StringFormat();
                 sf.LineAlignment = StringAlignment.Center;
@@ -30,24 +58,24 @@ namespace RainbowMage.OverlayPlugin
 
                 if (inc == SelectedIndex)
                 {
-                    e.Graphics.FillRectangle(new SolidBrush(SystemColors.Highlight), rect);
+                    e.Graphics.FillRectangle(new SolidBrush(SystemColors.Highlight), tabRect);
                     fore = SystemColors.HighlightText;
                     fontF = new Font(Font, FontStyle.Bold);
                 }
                 else
                 {
-                    e.Graphics.FillRectangle(Brushes.White, rect);
+                    e.Graphics.FillRectangle(Brushes.White, tabRect);
                 }
 
-                e.Graphics.DrawString(tp.Name, fontF, new SolidBrush(fore), textrect1, sf);
-                e.Graphics.DrawString(tp.Text, fontFSmall, new SolidBrush(fore), textrect2, sf);
+                e.Graphics.DrawString(tp.Name, fontF, new SolidBrush(fore), textRect1, sf);
+                e.Graphics.DrawString(tp.Text, fontFSmall, new SolidBrush(fore), textRect2, sf);
                 inc++;
             }
         }
 
-        protected override void OnTabIndexChanged(EventArgs e)
+        protected override void OnSelectedIndexChanged(EventArgs e)
         {
-            base.OnTabIndexChanged(e);
+            base.OnSelectedIndexChanged(e);
             Invalidate();
         }
 
@@ -63,7 +91,7 @@ namespace RainbowMage.OverlayPlugin
 
             DoubleBuffered = true;
 
-            ItemSize = new Size(46, 140);
+            //ItemSize = new Size(Dpi(46), Dpi(140));   // For some reason setting the size in the constructor is ineffective
             SizeMode = TabSizeMode.Fixed;
             BackColor = Color.Transparent;
         }
