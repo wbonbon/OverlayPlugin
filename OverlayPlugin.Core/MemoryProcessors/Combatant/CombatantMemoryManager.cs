22ac2541d3979abf6927dc0857b3efa26fa62754
valarnin <valarnin@gmail.com>
2025-03-25 19:40:47 -0400

    Add Combatant struct for 7.2 (#452)

--- OverlayPlugin.Core/MemoryProcessors/Combatant/CombatantMemoryManager.cs
+++ OverlayPlugin.Core/MemoryProcessors/Combatant/CombatantMemoryManager.cs
@@ -23,6 +23,7 @@ namespace RainbowMage.OverlayPlugin.MemoryProcessors.Combatant
             this.container = container;
             container.Register<ICombatantMemory70, CombatantMemory70>();
             container.Register<ICombatantMemory71, CombatantMemory71>();
+            container.Register<ICombatantMemory72, CombatantMemory72>();
             repository = container.Resolve<FFXIVRepository>();
 
             var memory = container.Resolve<FFXIVMemory>();
@@ -44,6 +45,7 @@ namespace RainbowMage.OverlayPlugin.MemoryProcessors.Combatant
             List<ICombatantMemory> candidates = new List<ICombatantMemory>();
             candidates.Add(container.Resolve<ICombatantMemory70>());
             candidates.Add(container.Resolve<ICombatantMemory71>());
+            candidates.Add(container.Resolve<ICombatantMemory72>());
             memory = FFXIVMemory.FindCandidate(candidates, repository.GetMachinaRegion());
         }
 
